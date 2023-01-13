9caa95cbccdb488261a38bf0755792dbb06ae76b
IoaMer <ioa.mertzanakis@gmail.com>
2023-01-13 18:15:05 +0200

    Added Candidate model and helping models

---
+++ ModelLibrary/ModelLibrary/Models/Candidate/Candidate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary.Models.Candidate
+{
+    internal class Candidate
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfbirth { get; set; }
+        public string Email { get; set; }
+        public string Landline { get; set; }
+        public string Mobile { get; set; }
+        public string CandidateNumber { get; set; }
+        public  string PhotoIdNumber { get; set; }
+        public DateTime PhotoIdIssueDate { get; set; }
+        public virtual Gender Gender { get; set; }
+        public virtual Language Language { get; set; }
+        public virtual PhotoIdType PhotoIdType{ get; set; }
+        public Address Address { get; set; }
+
+
+    }
+}
---
+++ ModelLibrary/ModelLibrary/Models/Candidate/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary.Models.Candidate
+{
+    public enum Country
+    {
+        None,
+        Greece,
+        England,
+        Albania,
+        Spain,
+
+    }
+}
