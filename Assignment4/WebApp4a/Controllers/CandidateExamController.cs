0474d430544238e741cdbb3af293900288476282
Vlasios Mavraganis <vlasisvv@outlook.com>
2023-01-15 16:55:35 +0200

    Candidate can sumbit the answers and the results pages is displayed right after

---
+++ Assignment4/WebApp4a/Controllers/CandidateExamController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelLibrary.Models.Exams;
+using WebApp4a.Data;
+
+namespace WebApp4a.Controllers
+{
+    public class CandidateExamController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CandidateExamController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(CandidateExam candidateExam)
+        {
+            ViewBag.Title = "Results";
+            return await Task.Run(() =>View(candidateExam));
+        }
+    }
+}
--- Assignment4/WebApp4a/Controllers/ExaminationController.cs
+++ Assignment4/WebApp4a/Controllers/ExaminationController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
 using ModelLibrary.Models.Candidates;
 using ModelLibrary.Models.Exams;
 using ModelLibrary.Models.Questions;
@@ -17,6 +19,7 @@ namespace WebApp4a.Controllers
     public class ExaminationController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private static int _examId;
 
         public ExaminationController(ApplicationDbContext context)
         {
@@ -36,27 +39,30 @@ namespace WebApp4a.Controllers
             //    return NotFound();
             //}            
 
-            return View(await GetAllQuestionsAsync(1));
+            _examId = 1;// Need Id from John
+            return View(await GetAllQuestionsAsync());
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> GetAnswers(IEnumerable<string> hiddens)
+        public async Task<IActionResult> GetAnswers(IEnumerable<bool> DropDownOptions)
         {
             if (ModelState.IsValid)
             {
-                
+                var candidateExam = await AddCandidateExam(DropDownOptions);
+
+                return await Task.Run(() => RedirectToAction("Index", "CandidateExam", candidateExam));
             }
 
-            return await Task.Run(() => View(hiddens));
+            return await Task.Run(() => RedirectToAction("Exam"));
         }
 
-        private async Task<IEnumerable<Question>> GetAllQuestionsAsync(int examId)
+        private async Task<IEnumerable<Question>> GetAllQuestionsAsync()
         {
             var exam = await Task.Run(() => _context.Exams
                 .Include(p => p.Questions)
                 .ThenInclude(p => p.Options)
-                .Where(p => p.Id == examId).SingleOrDefault());
+                .Where(p => p.Id == _examId).SingleOrDefault());
 
             if (exam != null)
             {
@@ -73,6 +79,46 @@ namespace WebApp4a.Controllers
             {
                 return Enumerable.Empty<Question>();
             }
+        }
+
+        private async Task<CandidateExam> AddCandidateExam(IEnumerable<bool> dropDownOptions)
+        {
+            bool passed = false;
+            int candScore = 0;
+
+            var candidateExam = new CandidateExam
+            {
+                Result = passed,
+                MaxScore = 100,
+                PercentScore = 100,
+                ExamDate = DateAndTime.Now,
+                ReportDate = DateAndTime.Now,
+                AssessmentCode = "CB"
+            };
+
+            _context.CandidateExams.Add(candidateExam);
+
+            foreach (var item in dropDownOptions)
+            {
+                if (item == true)
+                {
+                    candScore++;
+                }
+
+                var examAnswers = new CandidateExamAnswers
+                {
+                    CorrectOption = "correct",
+                    ChosenOption = "choosen",
+                    IsCorrect = item,
+                    CandidateExam = candidateExam
+                };
+
+                _context.CandidateExamAnswers.Add(examAnswers);
+            }
+
+            candidateExam.CandidateScore = candScore;
+            await _context.SaveChangesAsync();
+            return candidateExam;
         }
     }
 }
