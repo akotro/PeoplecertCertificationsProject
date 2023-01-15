be34a916a5359ef05ecb2bf00311b94b2bdf680f
AKotro <akotronakis@hotmail.com>
2023-01-15 18:32:02 +0200

    full questions crud with difficulty and topics

--- Assignment4/WebApp4a/Controllers/QuestionsController.cs
+++ Assignment4/WebApp4a/Controllers/QuestionsController.cs
@@ -20,76 +20,6 @@ namespace WebApp4a.Controllers
             _context = context;
         }
 
-        // GET: Questions
-        public async Task<IActionResult> Index()
-        {
-            return _context.Question != null
-                ? View(await _context.Question.ToListAsync())
-                : Problem("Entity set 'ApplicationDbContext.Question'  is null.");
-        }
-
-        // GET: Questions/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Question == null)
-            {
-                return NotFound();
-            }
-
-            var question = await _context.Question
-                .Include(q => q.DifficultyLevel)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (question == null)
-            {
-                return NotFound();
-            }
-
-            return View(question);
-        }
-
-        // GET: Questions/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Questions/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Text,DifficultyLevel")] Question question)
-        {
-            if (ModelState.IsValid)
-            {
-                question.DifficultyLevel = GetDifficultyLevel(question);
-                _context.Add(question);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-
-            return View(question);
-        }
-
-        // GET: Questions/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Question == null)
-            {
-                return NotFound();
-            }
-
-            var question = await _context.Question
-                .Include(q => q.DifficultyLevel)
-                .FirstOrDefaultAsync(q => q.Id == id);
-            if (question == null)
-            {
-                return NotFound();
-            }
-
-            return View(question);
-        }
-
         private DifficultyLevel? GetDifficultyLevel(Question question)
         {
             // TODO:(akotro) This belongs to a QuestionsRepository
@@ -127,6 +57,136 @@ namespace WebApp4a.Controllers
             return result;
         }
 
+        private List<SelectListItem>? GetTopicSelectList(Question? question)
+        {
+            // NOTE:(akotro) Is this Include needed here?
+            // var topics = _context.Topics.Include(t => t.Certificates).ToList();
+            var topics = _context.Topics.ToList();
+            if (topics == null || topics.Count <= 0)
+                return null;
+
+            var result = new List<SelectListItem>();
+            var group = new SelectListGroup();
+
+            foreach (var topic in topics)
+            {
+                var selectListItem = new SelectListItem()
+                {
+                    Disabled = false,
+                    Group = group,
+                    Selected = false,
+                    Text = $@"{topic.Name}",
+                    Value = topic.Id.ToString()
+                };
+
+                if (question?.Topic == topic)
+                {
+                    selectListItem.Selected = true;
+                }
+
+                result.Add(selectListItem);
+            }
+
+            if (question == null)
+            {
+                result.ElementAt(0).Selected = true;
+            }
+
+            return result;
+        }
+
+        private void PopulateQuestion(Question question, int topicId)
+        {
+            question.DifficultyLevel = GetDifficultyLevel(question);
+            question.Topic = _context.Topics.Find(topicId);
+        }
+
+        // GET: Questions
+        public async Task<IActionResult> Index()
+        {
+            return _context.Question != null
+                ? View(
+                    await _context.Question
+                        .Include(q => q.DifficultyLevel)
+                        .Include(q => q.Topic)
+                        .ToListAsync()
+                )
+                : Problem("Entity set 'ApplicationDbContext.Question'  is null.");
+        }
+
+        // GET: Questions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Question == null)
+            {
+                return NotFound();
+            }
+
+            var question = await _context.Question
+                .Include(q => q.DifficultyLevel)
+                .Include(q => q.Topic)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return View(question);
+        }
+
+        // GET: Questions/Create
+        public IActionResult Create()
+        {
+            ViewData.Add("Topic", GetTopicSelectList(null));
+
+            return View();
+        }
+
+        // POST: Questions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(
+            [Bind("Id,Text,DifficultyLevel")] Question question,
+            [Bind("Topic")] int topic
+        )
+        {
+            if (ModelState.IsValid)
+            {
+                PopulateQuestion(question, topic);
+
+                _context.Add(question);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(question);
+        }
+
+        // GET: Questions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Question == null)
+            {
+                return NotFound();
+            }
+
+            var question = await _context.Question
+                .Include(q => q.DifficultyLevel)
+                .Include(q => q.Topic)
+                .FirstOrDefaultAsync(q => q.Id == id);
+
+            ViewData.Add("Topic", GetTopicSelectList(question));
+
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return View(question);
+        }
+
         // POST: Questions/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -134,7 +194,8 @@ namespace WebApp4a.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(
             int id,
-            [Bind("Id,Text,DifficultyLevel")] Question question
+            [Bind("Id,Text,DifficultyLevel")] Question question,
+            [Bind("Topic")] int topic
         )
         {
             if (id != question.Id)
@@ -146,7 +207,8 @@ namespace WebApp4a.Controllers
             {
                 try
                 {
-                    question.DifficultyLevel = GetDifficultyLevel(question);
+                    PopulateQuestion(question, topic);
+
                     _context.Update(question);
                     await _context.SaveChangesAsync();
                 }
