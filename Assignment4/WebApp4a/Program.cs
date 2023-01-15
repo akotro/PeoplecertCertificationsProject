d3e8232e2997097ce7361f4c0dca90afc1dd9ea3
AKotro <akotronakis@hotmail.com>
2023-01-15 19:26:12 +0200

    renamed Question table to Questions + fixed navbar links

--- Assignment4/WebApp4a/Program.cs
+++ Assignment4/WebApp4a/Program.cs
@@ -12,12 +12,20 @@ namespace WebApp4a
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+            var connectionString =
+                builder.Configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException(
+                    "Connection string 'DefaultConnection' not found."
+                );
+            builder.Services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(connectionString)
+            );
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = false)
+            builder.Services
+                .AddDefaultIdentity<AppUser>(
+                    options => options.SignIn.RequireConfirmedAccount = false
+                )
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddRazorPages();
             builder.Services.AddMvc();
