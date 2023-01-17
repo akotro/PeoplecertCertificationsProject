7945feefec86da628a05e1effb9408b51a3ede21
AKotro <akotronakis@hotmail.com>
2023-01-17 12:51:28 +0200

    changed back to defaultconnection string

--- Assignment4/WebApp4a/Program.cs
+++ Assignment4/WebApp4a/Program.cs
@@ -16,7 +16,7 @@ namespace WebApp4a
 
             // Add services to the container.
             var connectionString =
-                builder.Configuration.GetConnectionString("akotro")
+                builder.Configuration.GetConnectionString("DefaultConnection")
                 ?? throw new InvalidOperationException(
                     "Connection string 'DefaultConnection' not found."
                 );
