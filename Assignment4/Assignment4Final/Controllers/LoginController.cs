c31d15888f18769c352ffc7ff177384006c08a64
AKotro <akotronakis@hotmail.com>
2023-01-30 13:31:33 +0200

    simple login api controller working (need to test roles)

--- Assignment4/Assignment4Final/Controllers/LoginController.cs
+++ Assignment4/Assignment4Final/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -11,27 +12,29 @@ using ModelLibrary.Models.DTO.Login;
 
 namespace Assignment4Final.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class LoginController : ControllerBase
     {
         private readonly SignInManager<AppUser> _signInManager;
-        private readonly UserManager<AppUser> _userManager;
+
+        // private readonly UserManager<AppUser> _userManager;
         private readonly IMapper _mapper;
 
         public LoginController(
             SignInManager<AppUser> signInManager,
-            UserManager<AppUser> userManager,
+            // UserManager<AppUser> userManager,
             IMapper mapper
         )
         {
             _signInManager = signInManager;
-            _userManager = userManager;
+            // _userManager = userManager;
             _mapper = mapper;
         }
 
+        [AllowAnonymous]
         [HttpPost]
-        // [AllowAnonymous]
         public async Task<IActionResult> Login(LoginDto loginDto)
         {
             var user = await _signInManager.UserManager.FindByNameAsync(loginDto.Username);
@@ -44,13 +47,20 @@ namespace Assignment4Final.Controllers
 
                 var userDto = _mapper.Map<UserDto>(user);
 
-                var roles = await _userManager.GetRolesAsync(user);
+                var roles = await _signInManager.UserManager.GetRolesAsync(user);
                 userDto.Roles = roles.ToList();
 
                 return Ok(userDto);
             }
 
             return BadRequest("Username or password is incorrect");
+        }
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return NoContent();
         }
     }
 }
