945c5c4a4a8a7e1fe11947d9f3957d16b7380cbf
Andreas Gkizis <andreasgkizis@gmail.com>
2023-01-31 22:58:34 +0200

    in the middle

---
+++ Assignment4/Assignment4Final/ClientApp/src/components/Candidate_Page/CandidateHomepage.js
@@ -0,0 +1,23 @@
+﻿import React, { useState } from 'react';
+
+const CandidateHomepage = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchCertificates = async () => {
+            try {
+                const response = await axios.get('https://localhost:7196/api/Certificates');
+                setProducts(response.data.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchCertificates();
+    }, []);
+
+    return (
+        <div>
+            List of certs
+        </div>
+    )
+}
