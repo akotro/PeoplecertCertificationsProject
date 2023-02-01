0b5abbbee187163b96adf2539d987085064d31b7
AKotro <akotronakis@hotmail.com>
2023-02-01 20:34:43 +0200

    fixed candidateexam get to use post + added newtonsoft.json serialization

--- Assignment4/Assignment4Final/Services/CandidateExamService.cs
+++ Assignment4/Assignment4Final/Services/CandidateExamService.cs
@@ -9,16 +9,19 @@ namespace Assignment4Final.Services
 {
     public class CandidateExamService
     {
-
         private readonly CandidateExamRepository _candidateExamRepository;
         private readonly ExamService _examService;
         private readonly IMapper _mapper;
 
-        public CandidateExamService(CandidateExamRepository candidateExamRepository,IMapper mapper, ExamService examService)
+        public CandidateExamService(
+            CandidateExamRepository candidateExamRepository,
+            IMapper mapper,
+            ExamService examService
+        )
         {
-            _candidateExamRepository= candidateExamRepository;
-            _mapper= mapper;
-            _examService = examService;    
+            _candidateExamRepository = candidateExamRepository;
+            _mapper = mapper;
+            _examService = examService;
         }
 
         public async Task<Candidate?> GetCandidateByUserId(string userId)
@@ -28,25 +31,26 @@ namespace Assignment4Final.Services
 
         public async Task<CandidateExam> GetCandidateExamByExam(Exam exam, string userId)
         {
-           var candidate = await GetCandidateByUserId(userId);
-            var examEntity = await _examService.GetExamAsync(exam.Id); 
-           var candidateExam = new CandidateExam() { Candidate = candidate , Exam = examEntity }; 
+            var candidate = await GetCandidateByUserId(userId);
+            var examEntity = await _examService.GetExamAsync(exam.Id);
+            var candidateExam = new CandidateExam()
+            {
+                Candidate = candidate,
+                Exam = examEntity,
+                Voucher = Guid.NewGuid().ToString()
+            };
             return candidateExam;
         }
 
-        public  void AddCandidateExam(ref CandidateExam candidateExam)
+        public void AddCandidateExam(ref CandidateExam candidateExam)
         {
-              _candidateExamRepository.Add(ref candidateExam);
-            
+            _candidateExamRepository.Add(ref candidateExam);
         }
+
         public CandidateExamDto GetCandidateExamDtoFromCandidateExam(CandidateExam candidateExam)
         {
             _candidateExamRepository.LoadCertificateOfCandidateExamEntity(ref candidateExam);
             return _mapper.Map<CandidateExamDto>(candidateExam);
         }
-
-
-
-
     }
 }
--- Assignment4/ModelLibrary/Models/DTO/Exam/ExamDto.cs
+++ Assignment4/ModelLibrary/Models/DTO/Exam/ExamDto.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ModelLibrary.Models.DTO.Certificates;
+using Newtonsoft.Json;
 
 namespace ModelLibrary.Models.DTO.Exams
 {
     public class ExamDto
     {
-
         public int Id { get; set; }
         public string? CertificateTitle { get; set; }
+
+        [JsonProperty(ReferenceLoopHandling = ReferenceLoopHandling.Ignore)]
+        public CertificateDto? Certificate { get; set; }
     }
 }
