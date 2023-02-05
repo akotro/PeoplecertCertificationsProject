ca16bc4d3a1843ce2c754d86ff99da139bc01b69
AKotro <akotronakis@hotmail.com>
2023-02-05 22:45:47 +0200

    exam api first pass + accounts api now returns userId in claims

--- Assignment4/Assignment4Final/Services/ExamService.cs
+++ Assignment4/Assignment4Final/Services/ExamService.cs
@@ -8,40 +8,75 @@ namespace Assignment4Final.Services
     public class ExamService
     {
         private readonly ExamRepository _repository;
+        private readonly ITopicsRepository _topicsRepository;
         private readonly IMapper _mapper;
 
-        public ExamService(ExamRepository repository, IMapper mapper)
+        public ExamService(
+            ExamRepository repository,
+            ITopicsRepository topicsRepository,
+            IMapper mapper
+        )
         {
             _repository = repository;
+            _topicsRepository = topicsRepository;
             _mapper = mapper;
         }
-        
+
         public async Task<List<Exam>> GetListOfExamsAsync()
         {
             return await _repository.GetAllExamsAsync();
         }
-
 
         public List<ExamDto> GetExamDtoList(List<Exam> exams)
         {
             return _mapper.Map<List<ExamDto>>(exams);
         }
 
-        
-
         public Exam GetExamFromExamDto(ExamDto examDto)
         {
             return _mapper.Map<Exam>(examDto);
         }
+
         public async Task<Exam?> GetExamAsync(int id)
         {
             return await _repository.GetExamAsync(id);
         }
 
+        public async Task<ExamDto?> GetAsync(int id)
+        {
+            var exam = await _repository.GetExamAsync(id);
+            return exam == null ? null : _mapper.Map<ExamDto>(exam);
+        }
 
+        public async Task<ExamDto?> AddAsync(ExamDto examDto)
+        {
+            var exam = _mapper.Map<Exam>(examDto);
 
+            // TODO:(akotro) Populate exam with random questions through topicsRepository?
+            var topics = await _topicsRepository.GetAllAsync();
+            var random = new Random();
+            exam.Questions = exam.Certificate.Topics
+                .Select(t => topics.First(x => x.Id == t.Id))
+                .SelectMany(t => t.Questions)
+                .OrderBy(x => random.Next())
+                .Take(5)
+                .ToList();
 
+            var addedExam = await _repository.AddAsync(exam);
+            return addedExam == null ? null : _mapper.Map<ExamDto>(addedExam);
+        }
 
+        public async Task<ExamDto?> UpdateAsync(int id, ExamDto examDto)
+        {
+            var exam = _mapper.Map<Exam>(examDto);
+            var updatedExam = await _repository.UpdateAsync(id, exam);
+            return updatedExam == null ? null : _mapper.Map<ExamDto>(updatedExam);
+        }
 
+        public async Task<ExamDto?> DeleteAsync(int id)
+        {
+            var deletedExam = await _repository.DeleteAsync(id);
+            return deletedExam == null ? null : _mapper.Map<ExamDto>(deletedExam);
+        }
     }
 }
