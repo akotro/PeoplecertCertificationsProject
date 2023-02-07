7238c004ebf7108fceb5d358ea9f0bfd5d18c2a6
AKotro <akotronakis@hotmail.com>
2023-02-07 23:06:17 +0200

    trying ckeditor stuff

--- Assignment4/Assignment4Final/ClientApp/src/components/Questions/Editor.js
+++ Assignment4/Assignment4Final/ClientApp/src/components/Questions/Editor.js
@@ -1,60 +1,91 @@
-import React from "react";
+// import React from "react";
+// import { CKEditor } from "@ckeditor/ckeditor5-react";
+// import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+// import SimpleUploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter";
 
+// const MyEditor = (props) => {
+//   const handleEditorReady = (editor) => {
+//     console.log("Editor is ready!", editor);
+//   };
+
+//   const handleChange = (event, editor) => {
+//     console.log("Editor data has changed!", editor.getData());
+//   };
+
+//   return (
+//     <CKEditor
+//       editor={ClassicEditor}
+//       config={{
+//         plugins: [SimpleUploadAdapter],
+//         simpleUpload: {
+//           uploadUrl: "https://localhost:7196/api/File",
+//         },
+//       }}
+//       data={props.initialData}
+//       onReady={handleEditorReady}
+//       onChange={handleChange}
+//     />
+//   );
+// };
+
+// export default MyEditor;
+
+import React from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-// import { SimpleUploadAdapter } from "@ckeditor/ckeditor5-upload";
-import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 
-function MyEditor(props) {
-    console.log("Before editor");
+const API_URL = "https://localhost:7196/api";
+const UPLOAD_ENDPOINT = "File";
 
-    return (
-        <div>
-            <CKEditor
-                editor={ClassicEditor}
-                config={{
-                    // plugins: [ Paragraph, Bold, Italic, Essentials ],
-                    // toolbar: [ 'bold', 'italic' ],
-                    plugins: [SimpleUploadAdapter],
-                    simpleUpload: {
-                        // The URL that the images are uploaded to.
-                        uploadUrl: "https://localhost:7196/api/File",
-                    },
-                }}
-                data="<p>Hello from the first editor working with the context!</p>"
-                onReady={(editor) => {
-                    // You can store the "editor" and use when it is needed.
-                    console.log("Editor1 is ready to use!", editor);
-                }}
-            />
-        </div>
-    );
+export default function MyEditor({ handleChange, ...props }) {
+  function uploadAdapter(loader) {
+    return {
+      upload: () => {
+        return new Promise((resolve, reject) => {
+          const body = new FormData();
+          loader.file.then((file) => {
+            body.append("files", file);
+            // let headers = new Headers();
+            // headers.append("Origin", "http://localhost:3000");
+            fetch(`${API_URL}/${UPLOAD_ENDPOINT}`, {
+              method: "post",
+              body: body
+              // mode: "no-cors"
+            })
+              .then((res) => res.json())
+              .then((res) => {
+                resolve({
+                  default: `${API_URL}/${UPLOAD_ENDPOINT}/image/${res.filename}`
+                });
+              })
+              .catch((err) => {
+                reject(err);
+              });
+          });
+        });
+      }
+    };
+  }
+  function uploadPlugin(editor) {
+    editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
+      return uploadAdapter(loader);
+    };
+  }
+  return (
+    <div className="App">
+      <CKEditor
+        config={{
+          extraPlugins: [uploadPlugin]
+        }}
+        editor={ClassicEditor}
+        onReady={(editor) => {}}
+        onBlur={(event, editor) => {}}
+        onFocus={(event, editor) => {}}
+        // onChange={(event, editor) => {
+        //   handleChange(editor.getData());
+        // }}
+        {...props}
+      />
+    </div>
+  );
 }
-
-export default MyEditor;
-
-// editor={EditorPlus}
-// data={question.text}
-// onReady={editor => {
-//   // You can store the "editor" and use when it is needed.
-// }}
-
-// onChange={(event, editor) => {
-//   const data = editor.getData();
-//   handleChange(data,event,editor);
-// }}
-// // config={{
-// //   simpleUpload: {
-// //     // The URL that the images are uploaded to.
-// //     uploadUrl: "https://localhost:44473/admin/Questions/QuestionCreate",
-
-// //     // Enable the XMLHttpRequest.withCredentials property if required.
-// //     withCredentials: true,
-
-// //     // Headers sent along with the XMLHttpRequest to the upload server.
-// //     headers: {
-// //       "X-CSRF-TOKEN": "CSFR-Token",
-// //       Authorization: "Bearer <JSON Web Token>"
-// //     }
-// //   }
-// // }}
