bcd5cb14c2e1a6d7c283f877373f227c966f08f0
AKotro <akotronakis@hotmail.com>
2023-02-11 00:13:42 +0200

    user crud working!

--- Assignment4/ModelLibrary/DTO/Accounts/UserDto.cs
+++ Assignment4/ModelLibrary/DTO/Accounts/UserDto.cs
@@ -1,10 +1,17 @@
-﻿namespace ModelLibrary.Models.DTO.Accounts;
+﻿using Newtonsoft.Json;
+
+namespace ModelLibrary.Models.DTO.Accounts;
 
 public class UserDto
 {
-    public string Id { get; set; }
+    public string? Id { get; set; }
     public string UserName { get; set; }
 
     public string Email { get; set; }
-    // public List<string> Roles { get; set; }
+    public string? PhoneNumber { get; set; }
+
+    public string? Role { get; set; }
+
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+    public LoginDto? Credentials { get; set; }
 }
